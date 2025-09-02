a61821ac1d35d665d6b869fef7cd8e5079339206
Sebastiaan van der Wilt <745982@student.inholland.nl>
2025-09-02 10:13:11 +0200

    Assignment2 finished

---
+++ assignment2/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace assignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // asks and reads the age of the user and stores it in the variable "age"
+            Console.WriteLine("Enter your age: ");
+            string input = Console.ReadLine();
+
+            //convert text to integer
+            int age = int.Parse(input);
+
+            //now we can perform some (arithmetic) operations on the (int) age
+            //(Which are not possible on the string value)
+            age = age + 1;
+
+            //convert integer value to text
+            string output = age.ToString();
+
+            // display the increased age
+            Console.WriteLine("Next year you will be " + output + " years old.");
+
+            //wait for the user to enter a key
+            Console.ReadKey();
+        }
+    }
+}
