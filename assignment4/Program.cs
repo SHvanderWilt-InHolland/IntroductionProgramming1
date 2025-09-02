0510c4b97195c99ade6e3d54e44b9887849da6df
Sebastiaan van der Wilt <745982@student.inholland.nl>
2025-09-02 10:33:37 +0200

    Assignment4 finished

---
+++ assignment4/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace assignment4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // read a number
+            Console.WriteLine("Enter a number: ");
+            string input = Console.ReadLine(); 
+            int number = int.Parse(input);
+
+            // assign variable "result" based on the value of "number + number"
+            int result = number + number;
+
+            // add 10 to number
+            result = result + 10;
+
+            // divide result by 2 (use /= operator)
+            result /= 2;
+
+            // subtract original number from result (use -= operator)
+            result -= number;
+
+            Console.WriteLine($"Result is: {result}");
+        }
+    }
+}
