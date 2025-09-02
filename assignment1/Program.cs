d0f742384e2209fe90f8ae4210c0f874e443ddf9
Sebastiaan van der Wilt <745982@student.inholland.nl>
2025-09-02 09:50:42 +0200

    Add project files.

---
+++ assignment1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // variable "name" is declared here
+            string name;
+
+            // reads the name of the user and stores it in the variable "name"
+            Console.WriteLine("Enter your name: ");
+            name = Console.ReadLine();
+
+            // reads the age of the user and stores it in the variable "age"
+            Console.WriteLine("Enter your age: ");
+            string age = Console.ReadLine();
+
+            // display name and age
+            Console.WriteLine("Your name is: " + name);
+            Console.WriteLine("Your age is: " + age);
+
+            //wait for the user to enter a key
+            Console.ReadKey();
+        }
+    }
+}
