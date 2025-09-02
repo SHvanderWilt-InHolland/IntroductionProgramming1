1b8fdd22e6088f9546249c5a61f6ea1b9072898e
Sebastiaan van der Wilt <745982@student.inholland.nl>
2025-09-02 10:21:28 +0200

    Ass1gnment3 finished

--- assignment3/Program.cs
+++ assignment3/Program.cs
@@ -1,10 +1,28 @@
 ﻿namespace assignment3
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //read first name of user
+            Console.WriteLine("Enter your first name: ");
+            string firstName = Console.ReadLine();
+
+            //read last name of user
+            Console.WriteLine("Enter your last name: ");
+            string lastName = Console.ReadLine();
+
+            //display full name (option 1)
+            Console.WriteLine("Your full name is: " + firstName + " " + lastName);
+
+            //display full name (option 2)
+            Console.WriteLine("Your full name is {0} {1}", firstName, lastName);
+
+            //display full name (option 3)
+            Console.WriteLine($"Your full name is: {firstName} {lastName}");
+
+            //wait for the user to enter a key
+            Console.ReadKey();
         }
     }
 }
